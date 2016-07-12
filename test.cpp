5bebaf4b4b55d7b72b47973fbc92346e4cd11496
bjwushaoyun <bjwushaoyun@corp.netease.com>
2016-07-12 17:26:25 +0800

    add test file

---
+++ test.cpp
@@ -0,0 +1,21 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2016 163.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file test.cpp
+ * @author bjwushaoyun(bjwushaoyun@corp.netease.com)
+ * @date 2016/07/12 17:26:03
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+int main() {
+    return 0;
+}
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
