124d610f6798b8135e0f5c0acb71d3529bad5e23
bjwushaoyun <bjwushaoyun@corp.netease.com>
2016-07-26 11:05:25 +0800

    modify float_multi

--- float_multi/main.cpp
+++ float_multi/main.cpp
@@ -21,10 +21,14 @@ int main() {
 
     double a = 1.093726;
     double b = 2.827862;
+    double c;
 
-    std::cout << "a * b = " << a * b << std::endl;
-    printf("a * b = %.5f\n", a * b);
-    printf("a * b = %.6f\n", a * b);
+    c = a * b;
+
+    std::cout << "a * b = " << c << std::endl;
+    printf("a * b = %f\n", c);
+    printf("a * b = %.5f\n", c);
+    printf("a * b = %.6f\n", c);
 
     return 0;
 }
