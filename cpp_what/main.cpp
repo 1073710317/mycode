8fa7631aea9e36b9866c93bc396c31391bc74898
bjwushaoyun <bjwushaoyun@corp.netease.com>
2016-08-03 10:08:33 +0800

    add overload_vir_nonvir

---
+++ cpp_what/main.cpp
@@ -0,0 +1,32 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2016 163.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file main.cpp
+ * @author bjwushaoyun(bjwushaoyun@corp.netease.com)
+ * @date 2016/07/29 20:17:45
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+#include <iostream>
+
+template <class T, const char* str>
+class TestClass {
+    TestClass(const T& t) {
+        std::cout << str << std::endl;
+    }
+};
+
+int main() {
+    const char* str = "hello";
+    TestClass<int, "hello"> t_c;
+    return 0;
+}
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
---
+++ overload_vir_nonvir/main.cpp
@@ -0,0 +1,61 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2016 163.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file main.cpp
+ * @author bjwushaoyun(bjwushaoyun@corp.netease.com)
+ * @date 2016/08/03 10:02:12
+ * @version $Revision$ 
+ * @brief 参考readme
+ *  
+ **/
+
+#include <iostream>
+
+class A {  
+public:
+    virtual void fun() {
+        std::cout << "A::fun" << std::endl;
+    }
+};
+
+class AP : A {
+public:
+    virtual void fun() {
+        std::cout << "AP::fun" << std::endl;
+    }
+};
+
+class B {
+public:
+    void fun() {
+        std::cout << "B::fun" << std::endl;
+    }
+};
+
+class BP : B {  
+public:  
+    void fun() {
+        std::cout << "BP::fun" << std::endl;
+    }
+};  
+  
+int main(int argc, char** argv) {
+    A a; 
+    a.fun();    // A::fun
+    AP ap;  
+    ((A*)&ap)->fun(); // AP::fun 
+
+    B b;
+    b.fun(); // B::fun 
+    BP bp;  
+    ((B*)&bp)->fun();   // B::fun
+
+    return 0;  
+}  
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
