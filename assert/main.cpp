2d15fcab2b231b84ae639fc3eccecbdd0b25fe48
bjwushaoyun <bjwushaoyun@corp.netease.com>
2016-08-06 21:50:24 +0800

    add assert

---
+++ assert/main.cpp
@@ -0,0 +1,25 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2016 163.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file main.cpp
+ * @author bjwushaoyun(bjwushaoyun@corp.netease.com)
+ * @date 2016/08/06 21:39:10
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+#include <iostream>
+#include <cassert>
+
+int main() {
+    assert(0 > 1);  // receive signal 6, then abort() and core dump
+    return 0;
+}
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
